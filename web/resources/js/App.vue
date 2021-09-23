576ebdf5579973a42ced7e09b83fff2eb4f6e36f
pepsi999cola <edge999standard@gmail.com>
2021-09-24 00:20:15 +0900

    バグ修正

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -14,44 +14,43 @@
 </template>
 
 <script>
-import Message from './components/Message.vue'
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
-import { NOT_FOUND,UNAUTHORIZED, INTERNAL_SERVER_ERROR } from './util'
-
+import Message from "./components/Message.vue";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import { NOT_FOUND, UNAUTHORIZED, INTERNAL_SERVER_ERROR } from "./util";
 
 export default {
   components: {
     Message,
     Navbar,
-    Footer
+    Footer,
   },
   computed: {
-    errorCode () {
-      return this.$store.state.error.code
-    }
+    errorCode() {
+      return this.$store.state.error.code;
+    },
   },
   watch: {
     errorCode: {
-      handler (val) {
+      async handler(val) {
         if (val === INTERNAL_SERVER_ERROR) {
-          this.$router.push('/500')
+          this.$router.push("/500");
         } else if (val === UNAUTHORIZED) {
           // トークンをリフレッシュ
-          await axios.get('/api/refresh-token')
+          await axios.get("/api/refresh-token");
           // ストアのuserをクリア
-          this.$store.commit('auth/setUser', null)
+          this.$store.commit("auth/setUser", null);
           // ログイン画面へ
-          this.$router.push('/login')
+          this.$router.push("/login");
         } else if (val === NOT_FOUND) {
-          this.$router.push('/not-found')
+          this.$router.push("/not-found");
         }
       },
-      immediate: true
+      immediate: true,
     },
-    $route () {
-      this.$store.commit('error/setCode', null)
-    }
-  }
-}
+    $route() {
+      this.$store.commit("error/setCode", null);
+    },
+  },
+};
 </script>
